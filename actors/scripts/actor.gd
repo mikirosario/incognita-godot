189d9451cf6daf3479378fd76ac5136e910ebca5
Miki Rosario <chisparroja@gmail.com>
2026-01-04 11:25:07 +0100

    UI mode scaffolding.

--- actors/scripts/actor.gd
+++ actors/scripts/actor.gd
@@ -12,5 +12,8 @@ func initialize(actor_data: ActorData) -> bool:
 	body.apply_data(data)
 	return true
 
+func set_ui_mode(val: bool):
+	body.set_ui_mode(val)
+
 func _ready() -> void:
 	initialize(data) # Debug code: initialize should eventually be called by scene loader, and _initialized flag checked and handled
--- actors/scripts/actor_body.gd
+++ actors/scripts/actor_body.gd
@@ -3,18 +3,24 @@ extends CharacterBody2D
 
 @onready var animator: ActorAnimator = $AnimatedSprite2D
 var speed: float = 1.0
+var _input_enabled: bool = true
 
 func apply_data(data: ActorData) -> void:
 	speed = data.speed
 
-func _physics_process(_delta: float) -> void:
-	var input_vector: Vector2 = Input.get_vector(
-		"move_left",
-		"move_right",
-		"move_up",
-		"move_down"
-	)
+func set_ui_mode(enabled: bool) -> void:
+	_input_enabled = !enabled
+	velocity = Vector2.ZERO
 
-	velocity = input_vector * speed
-	animator.update_animation(velocity)
-	move_and_slide()
+func _physics_process(_delta: float) -> void:
+	if _input_enabled:
+		var input_vector: Vector2 = Input.get_vector(
+			"move_left",
+			"move_right",
+			"move_up",
+			"move_down"
+		)
+		# velocity = input_vector.normalized() * speed
+		velocity = input_vector * speed
+		animator.update_animation(velocity)
+		move_and_slide()
